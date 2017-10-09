f1eb89bc9d01fb40f23b762e453ae7cbf220608d
EastmanJian <50935@qq.com>
2017-10-09 11:20:04 +0800

    add demo sample items to tiy dropdown

--- web/js/tiy.js
+++ web/js/tiy.js
@@ -6,8 +6,9 @@ var tiyCodeMirror;
 /**
  * When page initiates, do the following
  *  1) initiate codemirror (syntax highlight code editor)
- *  2) if there is a 'sample' GET parameter, load the html in the get parameter on priority.
- *  3) if there is no GET parameter, check if there is local storage.
+ *  2) load sample list
+ *  3) if there is a 'sample' GET parameter, load the html in the get parameter on priority.
+ *  4) if there is no GET parameter, check if there is local storage.
  *      If yes, load the testing script from local storage.
  *      If no, call loadSample(), it loads the default sample.
  */
@@ -19,6 +20,10 @@ function loadWindow() {
         styleActiveLine: true
     });
 
+    //load sample from demo list
+    loadJSON("https://eastmanjian.cn/blog/json/demo_list.json", updateDemoSamples);
+
+    //load target html codes
     var url = document.getElementById("sample").value;
     if (url == "null" && localStorage.codes) {
         tiyCodeMirror.setValue(localStorage.codes);
@@ -26,6 +31,46 @@ function loadWindow() {
     } else {
         loadSample();
     }
+}
+
+/**
+ * Callback function. Once the demo list json data arrives, append them into the option list dropdown box
+ * @param data - the demolist json data object
+ */
+function updateDemoSamples(data) {
+    var option;
+    var demoItem;
+    var urlList = document.getElementById("url_list");
+    //loop each prototype demo list
+    for (var p=0; p<data.prototypes.length; p++) {
+        for (var i=0; i<data.prototypes[p].list.length; i++) {
+            demoItem = data.prototypes[p].list[i]
+            if (demoItem[3] == "TIY") {
+                //add to load sample dropdown box if it uses TIY function
+                option = document.createElement("option");
+                option.setAttribute("label", data.prototypes[p].tech + " - " + demoItem[0] + " - " + demoItem[1]);
+                option.setAttribute("value", demoItem[2].replace("https://eastmanjian.cn/js_demo/", ""));
+                urlList.appendChild(option);
+            }
+        }
+    }
+}
+
+/**
+ * load json data with AJAX
+ * @param url - the json url
+ * @param callback - callback function after json is loaded, accept the json object as parameter
+ */
+function loadJSON(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onload = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var resp = JSON.parse(xmlhttp.responseText);
+            callback(resp);
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
 }
 
 
